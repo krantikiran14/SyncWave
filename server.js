0040d524dc28e13b0e538e81d5060fb57d074eaa
Krantikiran <82496778+krantikiran14@users.noreply.github.com>
2023-08-27 01:04:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3000 ; 
 
 const connectDB = require('./config/db');
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://harmonious-clafoutis-bb2680.netlify.app/'); // You can replace '*' with your specific domain
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.options('https://harmonious-clafoutis-bb2680.netlify.app/', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.send();
 });
 const corsOptions ={
     origin: process.env.ALLOWED_CLIENTS.split(',')
