ec0c893ee33e63f2ff84721facca7efd5e03a4c4
Krantikiran <82496778+krantikiran14@users.noreply.github.com>
2023-08-27 12:16:29 +0530

    Update emailTemplate.js

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -148,7 +148,7 @@ module.exports = ({emailFrom, downloadLink, size, expires}) => {
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
                                 <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">SyncWave inc.</span>
-                                <br> Want to share a file? <a href="http://localhost:3000" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">SyncWave</a>.
+                                <br> Want to share a file? <a href="https://harmonious-clafoutis-bb2680.netlify.app/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">SyncWave</a>.
                             </td>
                             </tr>
                             <tr>
